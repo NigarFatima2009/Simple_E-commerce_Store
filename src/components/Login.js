eb26989a2c66eaee96b6c000c56bf6cfedd88e8b
NigarFatima2009 <fatimanigar2554@gmail.com>
2024-06-25 11:35:39 +0500

    Final commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,16 @@
 // src/components/Login.js
+
 import React, { useContext, useState } from 'react';
 import { StoreContext } from '../context/StoreContext';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Login() {
   const { login } = useContext(StoreContext);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
@@ -21,9 +24,13 @@ function Login() {
     }
   };
 
+  const toggleShowPassword = () => {
+    setShowPassword(prevShowPassword => !prevShowPassword);
+  };
+
   return (
     <div className="container">
-      <div className = "login-div">
+      <div className="login-div">
         <h2>Login</h2>
         <input
           className="div-text1"
@@ -32,17 +39,22 @@ function Login() {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <br></br>
-        <br></br>
-        <input
-          className="div-text1"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br></br>
-        <br></br>
+        <br />
+        <br />
+        <div className="password-container">
+          <input
+            className="div-text1"
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <span className="password-toggle-icon" onClick={toggleShowPassword}>
+            <i className={showPassword ? "fas fa-eye-slash" : "fas fa-eye"}></i>
+          </span>
+        </div>
+        <br />
+        <br />
         <button
           className="div-button1"
           onClick={handleLogin}
@@ -51,7 +63,7 @@ function Login() {
           Login
         </button>
         {error && <p>{error}</p>}
-        </div>
+      </div>
     </div>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,16 @@
 // src/components/Register.js
+
 import React, { useContext, useState } from 'react';
 import { StoreContext } from '../context/StoreContext';
 import { useNavigate } from 'react-router-dom';
 import './Register.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Register() {
   const { register } = useContext(StoreContext);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const handleRegister = () => {
@@ -17,9 +20,13 @@ function Register() {
     navigate('/login');
   };
 
+  const toggleShowPassword = () => {
+    setShowPassword(prevShowPassword => !prevShowPassword);
+  };
+
   return (
     <div className="container">
-      <div className='Register-div'>
+      <div className="Register-div">
         <h2>Register</h2>
         <input
           className="div-text"
@@ -28,17 +35,22 @@ function Register() {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <br></br>
-        <br></br>
-        <input
-          className="div-text"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br></br>
-        <br></br>
+        <br />
+        <br />
+        <div className="password-container">
+          <input
+            className="div-text"
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <span className="password-toggle-icon" onClick={toggleShowPassword}>
+            <i className={showPassword ? "fas fa-eye-slash" : "fas fa-eye"}></i>
+          </span>
+        </div>
+        <br />
+        <br />
         <button
           className="div-button"
           onClick={handleRegister}
